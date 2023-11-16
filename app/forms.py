bfd059c0c151d2c8b3ebb66a29616075f76d2b66
ksthilaire13 <123573330+ksthilaire13@users.noreply.github.com>
2023-11-16 16:12:55 -0500

    another commit

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, IntegerField, SelectField, DateField, SelectMultipleField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
+from app.models import User, Run, Sleep
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -16,3 +18,16 @@ class RegistrationForm(FlaskForm):
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(user_email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+
+
