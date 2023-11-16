8d2b7dfc92cc0b2f1afff27955236ac961b95a6a
ksthilaire13 <123573330+ksthilaire13@users.noreply.github.com>
2023-11-16 16:01:25 -0500

    Secondary Commit

---
+++ app/reset.py
@@ -0,0 +1,63 @@
+import csv
+from werkzeug.security import generate_password_hash
+from app import db
+from app.models import User, Run, Sleep
+
+def reset_data():
+    # with app.app_context():  ## Causes this to run on startup
+    # clear all data from all tables
+    meta = db.metadata
+    for table in reversed(meta.sorted_tables):
+        print('Clear table {}'.format(table))
+        db.session.execute(table.delete())
+    db.session.commit()
+
+    ## Reload Users - needed for Foreign Keys
+    with open('database_scripts\\users.csv', 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            print(row)  # used for confirmation
+            user = User(
+                id=row['user_id'],
+                username=row['username'],
+                user_email=row['user_email'],
+                password_hash=generate_password_hash(row['user_password']),
+                name=row['name'],
+                bio=row['bio'],
+                photo=row['photo'],
+                date_registered =['date_registered'])
+            db.session.add(user)
+            db.session.commit()
+
+    ## Reload Artists
+    with open('database_scripts\\sleep.csv', 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            print(row)  # used for confirmation
+            sleep = Sleep(
+                sleep_id=row['sleep_id'],
+                date=row['date'],
+                bedtime=row['bedtime'],
+                wake_up=row['wake_up'],
+                times_awoken=row['times_awoken'],
+                dreams_torf=row['dreams_torf'],
+                sleep_notes=row['sleep_notes'],
+                user_id=row['user_id'])
+            db.session.add(sleep)
+            db.session.commit()
+
+    ## Reload Venues
+    with open('database_scripts\\runs.csv', 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            print(row)  # used for confirmation
+            run = Run(
+                venue_id=row['venue_id'],
+                venue_name=row['venue_name'],
+                venue_address=row['venue_address'],
+                venue_city=row['venue_city'],
+                venue_state=row['venue_state'],
+                venue_description=row['venue_description'],
+                max_capacity=row['max_capacity'])
+            db.session.add(run)
+            db.session.commit()
