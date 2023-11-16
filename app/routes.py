566dbfa432908c0088f0652a598cdec5a37521e4
Monty <yankeesfan4life18@gmail.com>
2023-11-16 16:01:58 -0500

    Main Page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from app import app
+from app import app, db
 from flask import render_template, redirect, url_for, flash, request
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import login_user, logout_user, current_user, login_required
 from app.models import User
 
@@ -8,14 +8,12 @@ from app.models import User
 @app.route('/')
 @app.route('/main')
 def main():
-    return render_template('base.html')
+    return render_template('main.html')
 
-
-@app.route('/')
 @app.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -25,14 +23,14 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page:
-            next_page = url_for('index')
+            next_page = url_for('main')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -42,3 +40,9 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main'))
