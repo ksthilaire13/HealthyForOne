26f38e937a35145465d3efb22a8006e09cc76771
ksthilaire13 <123573330+ksthilaire13@users.noreply.github.com>
2023-11-26 14:02:13 -0500

    created formulas.py and also edited run archives to include text that says what it is

---
+++ app/formulas.py
@@ -0,0 +1,13 @@
+def run_trend():
+    # uses past 2 runs to grade the trend of the run being evaluated
+    pass
+
+
+def sleep_trend():
+    # uses past 2 sleeps to grade the trend of the sleep being evaluated
+    pass
+
+
+def overall_grade():
+    # uses all the grades of the datapoint being evaluated to give an overall grade
+    pass
